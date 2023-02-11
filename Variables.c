51f8d60a01bac073fec223a7992016f50699352c
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2023-02-11 21:21:37 +0530

    Add files via upload

---
+++ Variables.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main(){
+	// Different types of Variables 
+//	int number=25;  // Integer type 
+//	printf("number : %d \n ",number);
+//	
+//	float pi=3.14;  // Float type
+//	char star="*";  // Character type 
+//	int age=22;
+//	age=24;       //to change the value of the variables. 
+//	
+//	int b=88;
+//	int B=89; // Variables are case sensitive.
+//	
+//	int _age=24; // Variables should always start with any alphabets or '_'.
+//	int final_price=100; // No Spaces or comma ',' between the variables only '_' can be used.
+	
+	
+	// Constants :- Variables which are fixed
+//	
+//	printf("print my name \n");
+	
+//	printf("pi : %f \n ",pi);  // To print the integer
+//	printf("star : %c \n",star);
+//	printf(" age : %d \n",_age);
+//	printf("b : %d \n",b);
+//	printf("B : %d \n",B);
+//	printf("final price : %d",final_price);
+//	
+
+//	int a;
+//	printf("enter a no.");
+//	scanf("%f",&a);
+//	printf("%f",a*3);
+	
+//	printf("print my name \n");
+//	printf("print anything \n");
+//	printf("KNFFEWFJJ FFJFJ FF \n");
+
+    // const int a=5;
+    // printf("%d",a);
+    int i=-3;
+	int l=i%2;
+	printf("%d",l);
+	
+	return 0;
+}
