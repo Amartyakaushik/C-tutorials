f6c48629c35867a2ea0adf330b14a3621f0b69ad
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2023-02-27 21:01:44 +0530

    Add files via upload

--- Hello.c
+++ Hello.c
@@ -813,25 +813,10 @@ int main() {
 		}
 	}
 
-//    int num, i;
-//    printf("Enter a number: ");
-//    scanf("%d", &num);
-//    printf("Factors of %d is : ", num);
-//    for (i = 1; i <= num; ++i) {     //after is incriment
-//        if (num % i == 0) {
-//            printf("%d ", i);
-//        }
-
-//    }
-
 
 
     return 0;
 }
-
-
-
-
 //	return 0;
 //}
 
