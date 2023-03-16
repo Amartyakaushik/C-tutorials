59419d2e94b308606dc65c249b21ee0a8fae6139
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2023-03-16 23:55:06 +0530

    Add files via upload

--- def.c
+++ def.c
@@ -1,5 +1,52 @@
-int callcount=0;
-void somefunction()
+//int callcount=0;
+//void somefunction()
+//{
+//	callcount++;
+////}
+
+#include<stdio.h>
+//void staticfun(){
+//	static int i;
+//	{
+//		static int i=1;
+//		printf("%d",i);
+//		i++;
+//	}
+//	printf("%d\n",i);
+//	i++;
+//}
+//int main(){
+//	staticfun();
+//	staticfun();
+//	return 0;
+//}
+
+//static int gint =1;
+//static void staticfun(){
+//	static int i;
+//	printf("%d",i);
+//	i++;
+//	printf("%d\n",gint);
+//	gint++;
+//}
+//int main (){
+//	gint++;
+//	staticfun();
+//	gint++;
+//	staticfun();
+//	return 0;
+//}
+
+int f(int n)
 {
-	callcount++;
+	static int i=1;
+	if (n>=5)
+	return n;
+	n=n+i;
+	i++;
+	return f(n);
+}
+int main(){
+	int k=f(1);
+	printf("%d",k);
 }
